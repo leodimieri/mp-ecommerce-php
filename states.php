91c49db253a47bd130305b8ec99504a23377c834
leodimieri <leodimieri@gmail.com>
2020-04-24 01:28:36 -0300

    try22

--- states.php
+++ states.php
@@ -75,7 +75,9 @@
             <div class="as-search-results as-filter-open as-category-landing as-desktop" id="as-search-results">
 
                 <div id="accessories-tab" class="as-accessories-details">
-
+                                <?php
+                                echo json_encode($_POST);
+                                ?>
                     <div class="as-accessories" id="as-accessories" style="height: 70vh; <?php if ($_POST['payment_status']!='approved') { echo "display:none;"; } ?>">
                             <div style="margin-top:20px;text-align:center;">
                                 <svg style="max-width:60px;max-height:60px;" xmlns="http://www.w3.org/2000/svg" height="512px" viewBox="0 0 512 512" width="512px"><g><path d="m369.164062 174.769531c7.8125 7.8125 7.8125 20.476563 0 28.285157l-134.171874 134.175781c-7.8125 7.808593-20.472657 7.808593-28.285157 0l-63.871093-63.875c-7.8125-7.808594-7.8125-20.472657 0-28.28125 7.808593-7.8125 20.472656-7.8125 28.28125 0l49.730468 49.730469 120.03125-120.035157c7.8125-7.808593 20.476563-7.808593 28.285156 0zm142.835938 81.230469c0 141.503906-114.515625 256-256 256-141.503906 0-256-114.515625-256-256 0-141.503906 114.515625-256 256-256 141.503906 0 256 114.515625 256 256zm-40 0c0-119.394531-96.621094-216-216-216-119.394531 0-216 96.621094-216 216 0 119.394531 96.621094 216 216 216 119.394531 0 216-96.621094 216-216zm0 0" data-original="#000000" class="active-path" data-old_color="#000000" fill="#69E781"/></g> </svg>
