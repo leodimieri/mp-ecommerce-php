95ffbc8fdc8544ee21fc5ccdbcab46531a468f83
leodimieri <leodimieri@gmail.com>
2020-04-24 00:52:39 -0300

    fixes

--- states.php
+++ states.php
@@ -67,20 +67,20 @@
                 <div id="accessories-tab" class="as-accessories-details">
 
                     <?php 
-                        echo json_encode($_POST);
+                        echo json_encode($_REQUEST);
                     ?>
 
-                    <div class="as-accessories" id="as-accessories" style="height: 70vh; <?php if ($_REQUEST['payment_status']!='approved') { echo "display:none;"; } ?>">
+                    <div class="as-accessories" id="as-accessories" style="height: 70vh; <?php if ($_POST['payment_status']!='approved') { echo "display:none;"; } ?>">
                             <div style="margin-top:20px;text-align:center;">
                                 <svg style="max-width:60px;max-height:60px;" xmlns="http://www.w3.org/2000/svg" height="512px" viewBox="0 0 512 512" width="512px"><g><path d="m369.164062 174.769531c7.8125 7.8125 7.8125 20.476563 0 28.285157l-134.171874 134.175781c-7.8125 7.808593-20.472657 7.808593-28.285157 0l-63.871093-63.875c-7.8125-7.808594-7.8125-20.472657 0-28.28125 7.808593-7.8125 20.472656-7.8125 28.28125 0l49.730468 49.730469 120.03125-120.035157c7.8125-7.808593 20.476563-7.808593 28.285156 0zm142.835938 81.230469c0 141.503906-114.515625 256-256 256-141.503906 0-256-114.515625-256-256 0-141.503906 114.515625-256 256-256 141.503906 0 256 114.515625 256 256zm-40 0c0-119.394531-96.621094-216-216-216-119.394531 0-216 96.621094-216 216 0 119.394531 96.621094 216 216 216 119.394531 0 216-96.621094 216-216zm0 0" data-original="#000000" class="active-path" data-old_color="#000000" fill="#69E781"/></g> </svg>
                             </div>
                             <center>
                                 <h2>¡Tu pago fue aprobado!</h2>
                                 <h4>
-                                    Pagaste: <b><?php echo "$" . $_REQUEST['transaction_amount'];?></b>,
-                                    con <b><?php echo $_REQUEST['payment_method_id']; ?></b> (<b><?php echo $_REQUEST['installments']; ?></b> cuota/s de <b><?php echo "$ " . $_REQUEST['transaction_details']['installment_amount']; ?></b>)<br>
-                                    Tu referencia para el pago es: <b><?php echo $_REQUEST['external_reference']; ?></b><br>
-                                    Tu ID del pago es: <b><?php echo $_REQUEST['id']; ?></b> (Conservalo para posibles revisiones)<br>
+                                    Pagaste: <b><?php echo "$" . $_POST['transaction_amount'];?></b>,
+                                    con <b><?php echo $_POST['payment_method_id']; ?></b> (<b><?php echo $_POST['installments']; ?></b> cuota/s de <b><?php echo "$ " . $_POST['transaction_details']['installment_amount']; ?></b>)<br>
+                                    Tu referencia para el pago es: <b><?php echo $_POST['external_reference']; ?></b><br>
+                                    Tu ID del pago es: <b><?php echo $_POST['id']; ?></b> (Conservalo para posibles revisiones)<br>
                                 </h4>
                                 <p><b>Muchas gracias por tu compra</b><br>
                                     En tu cuenta, en la sección de compras vas a poder ver el detalle y el estado de tu pedido.
@@ -91,7 +91,7 @@
                     </div>
 
 
-                    <div class="as-accessories" id="as-accessories" style="height: 70vh; <?php if ($_REQUEST['payment_status']!='rejected') { echo "display:none;"; } ?>">
+                    <div class="as-accessories" id="as-accessories" style="height: 70vh; <?php if ($_POST['payment_status']!='rejected') { echo "display:none;"; } ?>">
                             <div style="margin-top:20px;text-align:center;">
                             <svg style="max-width:60px;max-height:60px;" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve" width="512px" height="512px"><g><g>
                                 <g>
@@ -114,7 +114,7 @@
                     </div>
 
 
-                    <div class="as-accessories" id="as-accessories" style="height: 70vh; <?php if ($_REQUEST['payment_status']!='pending') { echo "display:none;"; } ?>">
+                    <div class="as-accessories" id="as-accessories" style="height: 70vh; <?php if ($_POST['payment_status']!='pending') { echo "display:none;"; } ?>">
                             <div style="margin-top:20px;text-align:center;">
                             <svg style="max-width:60px;max-height:60px;" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 60 60" style="enable-background:new 0 0 60 60;" xml:space="preserve" width="512px" height="512px"><g><g>
                                 <path d="M30,0C13.458,0,0,13.458,0,30s13.458,30,30,30s30-13.458,30-30S46.542,0,30,0z M30,58C14.561,58,2,45.439,2,30   S14.561,2,30,2s28,12.561,28,28S45.439,58,30,58z" data-original="#000000" class="active-path" data-old_color="#000000" fill="#009EE3"/>
