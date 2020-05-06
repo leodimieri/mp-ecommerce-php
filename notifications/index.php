89cf9bc26bb06a3dd63132a50568068b78659cdc
leodimieri <leodimieri@gmail.com>
2020-05-06 19:35:22 -0300

    fix notifications

--- notifications/index.php
+++ notifications/index.php
@@ -1,5 +1,30 @@
 <?php
 
-    echo true;
+    require __DIR__ .  '/vendor/autoload.php';
+
+    MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
+
+    switch($_POST["type"]) {
+        case "payment":
+            $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
+            break;
+        case "plan":
+            $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
+            break;
+        case "subscription":
+            $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
+            break;
+        case "invoice":
+            $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
+            break;
+    }
+
+    if(isset($payment)) {
+        return $payment;
+    } else if (isset($plan)) {
+        return $plan;
+    } else {
+        return true;
+    }
 
 ?>
