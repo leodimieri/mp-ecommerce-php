b109491044816c437cc1b160dc1a67a70d135835
leodimieri <leodimieri@gmail.com>
2020-05-13 19:53:25 -0300

    try2

--- detail.php
+++ detail.php
@@ -192,7 +192,7 @@
 
                                             </h3>
                                         </div>
-                                        <?php echo dd($preference) ?>      
+                                        <?php echo json_encode($preference); ?>      
                                         <h3 style="font-size:2em;">
                                            <b><?php echo "$" . $_POST['price'] ?> </b>
                                         </h3>
